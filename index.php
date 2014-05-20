8a11547918896c5e0047b7b710f067e7a5c2cc0f
thatoneguy150 <thatoneguy150@gmail.com>
2014-05-20 15:44:50 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html5>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>Puppy Database</title>
+</head>
+<body>
+
+<!-- This section should be in the body of the following IF statement if the GET method is being used, -->
+<!-- the action for that method is get_puppy, and an ID is specified. -->
+<!-- It should then show the following information about the puppy-->
+
+<h3>Here's more information about a specific puppy</h3>
+<table>
+<tr>
+<td>Puppy Name: </td><td><strong><!-- Echo the puppy's name here --></strong></td>
+</tr>
+<tr>
+<td>Puppy Age: </td><td><strong><!-- Echo the puppy's age here --></strong></td>
+</tr>
+<tr>
+<td>Puppy Color: </td><td><strong><!-- Echo the puppy's color here --></strong></td>
+</tr>
+<tr>
+<td>Puppy Breed: </td><td><strong><!-- Echo the puppy's breed here --></strong></td>
+</tr>
+<tr>
+<td>Puppy Sex: </td><td><strong><!-- Echo the puppy's sex here --></strong></td>
+</tr>
+</table>
+<br />
+<!-- Link back to the main list of puppies -->
+<!-- HINT: You shouldn't have to specify any URL parameters -->
+<a href="" alt="">Return to the puppy list</a>
+
+<!-- This section should be in the body of the ELSE statement. It should display the list of puppies with each list -->
+<!-- element displaying the name of the puppy as a link that should redirect the user to the above section -->
+
+<h3>Here is the list of puppies by name:</h3>
+<ul>
+<li>
+<!-- This link will redirect to the above section. Use the API to determine how it should do that -->
+<a href="" alt=""><!-- Puppy's name goes here --></a>
+</li>
+</ul>
+
+</body>
+</html>
